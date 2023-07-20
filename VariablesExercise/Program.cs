c352d02320d1cb7eabcdfa57f2d24cfd5126dc31
Mack McCall <mackmc9924@gmail.com>
2023-07-20 11:02:42 -0600

    added variables about coffee

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string coffeeName = "Marilyn Laucata";
+            string coffeeCountry = "Peru";
+            int weekAmt = 3;
+            char coffeeRank = 'A';
+            bool coffeeNerd = true;
+            double coffeeScore = 9.4;
+            decimal coffeeCost = 24.50m;
+
+            Console.WriteLine($"My favorite coffee in the past {weekAmt} weeks has been {coffeeName} from {coffeeCountry}. I would rank it a {coffeeScore} out of 10. That comes out to rank {coffeeRank}. You may think I am a coffee nerd. {coffeeNerd}. Only weird coffee people pay ${coffeeCost} for a bag of coffee.");
         }
     }
 }
